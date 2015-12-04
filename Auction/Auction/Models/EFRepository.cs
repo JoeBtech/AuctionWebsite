64ce22c1c8911ccd4a1778bef4304e1b38fe1acf
Joe B <joebtech@hotmail.com>
2015-12-04 11:06:17 -0800

    Created RFRepository and IRepository

---
+++ Auction/Auction/Models/EFRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction.Models {
+    class EFRepository : IRepository {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        public IList<AuctionItem> ListItems() {
+
+            return _db.AuctionItems.ToList();
+        }
+
+        public void SaveItem(AuctionItem itemToSave) {
+            if (itemToSave.Id == 0) {
+                _db.AuctionItems.Add(itemToSave);
+                _db.SaveChanges();
+            } else {
+                var original = this.Find(itemToSave.Id);
+                original.Name = itemToSave.Name;
+                original.Description = itemToSave.Description;
+                original.MinBid = itemToSave.MinBid;
+                _db.SaveChanges();
+            }
+        }
+
+        public AuctionItem Find(int id) {
+
+            return _db.AuctionItems.Find(id);
+        }
+
+        public void Delete(int id) {
+            var item = this.Find(id);
+            _db.AuctionItems.Remove(item);
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ Auction/Auction/Models/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Auction.Models {
+    interface IRepository {
+        void Delete(int id);
+        AuctionItem Find(int id);
+        IList<AuctionItem> ListItems();
+        void SaveItem(AuctionItem itemToSave);
+    }
+}
